868df04ad6ee8aa407aa1f442a755bdc6c107f48
Nurik <tanatov1989@mail.ru>
2018-02-02 17:12:21 +0600

    Добавьте файлы проекта.

---
+++ WpfApp7/UserControl1.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+      
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void Show_Click(object sender, RoutedEventArgs e)
+        {
+            name.Text = "picture.jpg";
+          
+            BitmapImage myBitmapImage1 = new BitmapImage();
+
+            myBitmapImage1.BeginInit();
+            myBitmapImage1.UriSource = new Uri(@"Z:\www\picture.jpg", UriKind.Absolute);
+            myBitmapImage1.EndInit();
+
+            JPG.Source = myBitmapImage1;
+        }
+       
+    }
+}
+
